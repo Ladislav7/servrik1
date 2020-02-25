47586832b1769c397260db5a4b93fa7316947a95
zak013BLH0 <hrblad@seznam.cz>
2020-02-25 14:57:04 +0100

    další věci

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const http = require("http");
 const fs = require("fs");
 
+let citac = 0;
+
 function main(req, res) { //hlavni funkcnost meho serveru
-    res.writeHead(200, {"Content-type":"text/html"});
-    let s = fs.readFileSync("index.html").toString();
-    console.log(s);
-    res.end(s);
+    console.log(req.url);
+    if (req.url == "/"){
+        res.writeHead(200, {"Content-type":"text/html"});
+        let s = fs.readFileSync("index.html");
+        res.end(s);
+    } else if (req.url == "/citac"){
+        citac++;
+        let obj = {};
+        obj.citac = citac;
+        obj.popis = "Muj prvni JSON ze serveru";
+        res.writeHead(200, {"Content-type":"application/json"});
+        res.end(JSON.stringify(obj));
+    } else {
+        res.writeHead(404, {"Content-type":"text/html"});
+        res.end();
+    }
 }
 
 let srv = http.createServer(main);
