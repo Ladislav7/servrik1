aa2bd14bb1fb508b02427c86902bdf7052fdd332
zak013BLH0 <hrblad@seznam.cz>
2020-03-03 15:03:05 +0100

    další věci

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 let citac = 0;
+let zpravy = new Array();
 
 function main(req, res) { //hlavni funkcnost meho serveru
     console.log(req.url);
@@ -16,12 +18,25 @@ function main(req, res) { //hlavni funkcnost meho serveru
         obj.popis = "Muj prvni JSON ze serveru";
         res.writeHead(200, {"Content-type":"application/json"});
         res.end(JSON.stringify(obj));
-    } else {
+    } else if (req.url.startsWith("/chat/list")){
+        let obj = {};
+        obj.messages = zpravy;
+        res.writeHead(200, {"Content-type":"application/json"});
+        res.end(JSON.stringify(obj));
+    } else if (req.url.startsWith("/chat/add")){
+        let q = url.parse(req.url, true);
+        console.log(q.query);
+        let s = q.query.msg;
+        zpravy.push(s);
+        let obj = {};
+        obj.messages = zpravy;
+        res.writeHead(200, {"Content-type":"application/json"});
+        res.end(JSON.stringify(obj));
+    }else {
         res.writeHead(404, {"Content-type":"text/html"});
         res.end();
     }
 }
-
 let srv = http.createServer(main);
 srv.listen(8080);
 
